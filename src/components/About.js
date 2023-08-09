244d36863d227bda66d06e53ea858516b03206ba
Andrew <a.oppman@gmail.com>
2023-08-09 01:04:43 -0400

    re-submit

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id="about">
+  return (
+  <div id="about">
     <h2>About Me</h2>
     <p>I'm cool bro!</p>
     <img src={ image } alt="I made this" />
-  </div>;
+  </div>
+  );
 }
 
 export default About;
